dd92d65a8a0c81e7290e6e66f7d3a22c90f72639
Roger-luo <hiroger@qq.com>
2018-03-12 02:24:58 +0800

    fix docs

--- docs/make.jl
+++ docs/make.jl
@@ -12,16 +12,9 @@ makedocs(
     ]
 )
 
-if "deploy" in ARGS
-
-    # Only deploy docs from 64bit Linux to avoid committing multiple versions of the same
-    # docs from different workers.
-    (Sys.ARCH === :x86_64 && Sys.KERNEL === :Linux) || return
-
-    deploydocs(
-        repo = "github.com/Roger-luo/QMTK.jl.git",
-        target = "build",
-        deps = nothing,
-        make = nothing,
-    )
-end
+deploydocs(
+    repo = "github.com/Roger-luo/QMTK.jl.git",
+    target = "build",
+    deps = nothing,
+    make = nothing,
+)

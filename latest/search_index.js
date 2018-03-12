97a8591b5516c26dc59673c748418d3f40b929f4
autodocs <autodocs>
2018-03-12 02:56:40 +0000

    build based on dd92d65

---
+++ latest/search_index.js
@@ -0,0 +1,43 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#QMTK.nn.Linear",
+    "page": "Home",
+    "title": "QMTK.nn.Linear",
+    "category": "type",
+    "text": "Linear{T, WeightType, BiasType, InputType} <: AbstractBlock\n\nLinear layer.\n\nz^l+1 = W^l * z^l + b^l\n\nexpression of gradients:\n\ndelta^l = (W^l)^Tdelta^l+1 delta^l_w = delta^l+1(z^l)^T delta^l_b = delta^l+1\n\n\n\n\n\n"
+},
+
+{
+    "location": "index.html#QMTK-1",
+    "page": "Home",
+    "title": "QMTK",
+    "category": "section",
+    "text": "Welcome to QMTK (Quantum Many-body Toolkit)The QMTK library is developed for physics system involves quantum many-body system, including condensed matter physics, quantum computing, quantum information and etc.In most numerical and theoretical works, programs related to quantum physics and quantum many-body system could have a lot similar functionality, methods, and objects. We aims to provide physicist with a powerful toolkit in Julia language as its frontend interface.nn.Linear"
+},
+
+{
+    "location": "man/Introduction.html#",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "man/Introduction.html#Introduction-1",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "section",
+    "text": "The QMTK package includes serveral parts:BasicType: Basic type definitions for quantum objects, e.g Basis, Sites, SubSites, etc.\nLattice: Lattice type definitions and related methods."
+},
+
+]}

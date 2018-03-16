3368a6917f2feb28f62514d7b67e55d466e0da84
Roger-luo <hiroger@qq.com>
2018-03-17 03:05:17 +0800

    fix macro kron

--- example/hamiltonian.jl
+++ example/hamiltonian.jl
@@ -43,3 +43,41 @@ h = @ham sum(:nearest, S * S) + sum(:nextnearest, S * S) + sum(:nearest, kron(si
 
 println(h)
 # Random = @ham sum(:rand, J(i,j) * S(i) * S(j))
+
+
+# J1J2
+# Chain: length 8, Fixed Boundary
+chain = Chain{Fixed}(4)
+
+itr = bonds(chain, 1)
+state = start(itr)
+(i, j), state = next(itr, state)
+local_expr(i, j) = :(σ₁[$i] * σ₁[$j] + σ₂[$i] * σ₂[$j] + σ₃[$i]*σ₃[$j])
+# local_expr(i, j) = :(σ₁[$i] * σ₁[$j])
+@show i, j
+ex = local_expr(i, j)
+
+while !done(itr, state)
+    (i, j), state = next(itr, state)
+    @show i, j
+    ex = :($(local_expr(i, j)) + $ex)
+end
+
+println(ex)
+# println(eval(toexpr(kronparse(ex))))
+# println(QMTK._kron(ex))
+
+
+h = @ham sum(:nearest, S * S) + sum(:nextnearest, S * S)
+
+# lh1 = LocalHamiltonian(Region{1}, kron(sigmax, sigmax))
+# lh2 = LocalHamiltonian(Region{2}, kron(sigmax, sigmax))
+chain = Chain{Fixed}(4)
+lhs = rand(Bit, 4)
+
+# h = Hamiltonian(lh1, lh2)
+
+for (val, rhs) in h(chain, lhs)
+    i = convert(Int, lhs) + 1
+    println(val, ", ", rhs)
+end

1d281b56e37c15213006e881df2d22f1f0047112
Roger <hiroger@qq.com>
2018-03-31 14:18:06 +0800

    add test for macro kron

--- test/Utils.jl
+++ test/Utils.jl
@@ -12,4 +12,12 @@ h = @kron sigmax[1] ⊗ sigmaz[3] + sigmax[2] ⊗ sigmay[4]
 ans = kronprod(σ₁, σ₀, σ₃, σ₀) + kronprod(σ₀, σ₁, σ₀, σ₂)
 @test h == ans
 
+h = @kron sigmax[1] * (sigmaz[2] + sigmay[4])
+ans = kronprod(σ₁, σ₃, σ₀, σ₀) + kronprod(σ₁, σ₀, σ₀, σ₂)
+@test h == ans
+
+h = @kron sigmax[5] * (sigmax[1] * (sigmaz[2] + sigmay[4]))
+ans = kronprod(σ₁, σ₃, σ₀, σ₀, σ₁) + kronprod(σ₁, σ₀, σ₀, σ₂, σ₁)
+@test h == ans
+
 end
